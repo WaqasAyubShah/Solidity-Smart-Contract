3dbd10d02a2b2f83e659e502a2bba2c7a9f8ae95
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2025-12-26 16:06:27 +0500

    Array update is added

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -15,11 +15,27 @@ contract Storage {
         uint256 fvrtNumber;
         string name;
     }
-    //Now lets call struct and save some data;
-    Person public jhon = Person({fvrtNumber: 5, name: "jhon"});
-    Person public wiki = Person({fvrtNumber: 7, name: "wiki"});
-    //The problem with struct is we have to call it again and again. 
 
+    //Dynamic arrays
+    Person[] public listofPeoples;
+
+    // //Now lets call struct and save some data;
+    // Person public jhon = Person({fvrtNumber: 5, name: "jhon"});
+    // Person public wiki = Person({fvrtNumber: 7, name: "wiki"});
+    // //The problem with struct is we have to call it again and again. 
+    // //to handle this we can use arrays.
+
+    //lets add data to array where we have two methodes. 
+    //One is calling the instant first and then passing it to the array
+    // function addPerson(uint256 _fvrtNumber, string memory _name) public{
+    //     Person memory newPerson = Person(_fvrtNumber , _name);
+    //     listofPeoples.push(newPerson);
+    // }
+    
+    //Second method to do it is directly pass the data
+    function addPerson(uint256 _fvrtNumber, string memory _name) public{
+        listofPeoples.push(Person(_fvrtNumber, _name));
+    }
     /**
      * @dev Store value in variable
      * @param num value to store
