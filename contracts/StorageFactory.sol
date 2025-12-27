bac77452f738428803c2b56cb5ed78232b4a4302
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2025-12-27 18:47:41 +0500

    Store and retrieve

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -6,9 +6,23 @@ import "./1_Storage.sol";
 
 contract StorageFactory{
     //Call the contract from other and you can deply it from calling the function...
-    Storage public myStorage;
+    //lets make an arry to be store if we deploy the contract multiple time..
+    Storage[] public mylistofStorageContract;
     //Start with somple function
     function CreateSampleStorageFactroy() public {
-        myStorage = new Storage();
+        Storage myNewStorage = new Storage();
+        mylistofStorageContract.push(myNewStorage);
+    }
+
+    //Now lets call the store function from storage function in the storagefactory
+    //To check contract, we need address and ABI
+    function sfStore(uint256 _samplestorageIndex, uint256 _samplestoragenumber ) public{
+        Storage myStorageinArray = mylistofStorageContract[_samplestorageIndex];
+        myStorageinArray.store(_samplestoragenumber);
+    }
+
+    function sfGet(uint256 _samplestorageIndex) public view returns(uint256){
+        Storage myStorage = mylistofStorageContract[_samplestorageIndex];
+        return myStorage.retrieve();
     }
 }
