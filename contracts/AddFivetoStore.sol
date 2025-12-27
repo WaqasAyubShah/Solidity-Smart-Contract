4b8db0a83594adc8ebeb38b21754ec3c403b3603
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2025-12-27 19:03:47 +0500

    hello from child contract

---
+++ contracts/AddFivetoStore.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import {Storage} from "./1_Storage.sol";
+
+contract AddFivetoStore is Storage{
+    //let say hello to the old Storage contract first
+    function sayHello() public pure returns(string memory){
+        return "Hello";
+    }
+
+}
